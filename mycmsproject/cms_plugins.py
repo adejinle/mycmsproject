761bca9aa4d5c37b53a7da794e85f7b0e9b87817
adejinle <adeo.adeniji@gmail>
2024-03-14 11:56:24 +0100

    thursday, march14

---
+++ mycmsproject/cms_plugins.py
@@ -0,0 +1,14 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from cms.models.pluginmodel import CMSPlugin
+from django.utils.translation import gettext_lazy as _
+
+@plugin_pool.register_plugin
+class Poetry(CMSPluginBase):
+    model = CMSPlugin
+    render_template = "mypoems.html"
+    cache = False
+
+
+
+
